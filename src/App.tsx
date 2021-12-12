f4d0def6cff5b0bd6c4be0891be1392982f31747
Jean Meira <jean.meira10@hotmail.com>
2021-12-12 20:59:07 -0300

    Add files and settings for project with typeScript

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="conteiner">
+        <div className="content">
+          <h1>Hello world</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
